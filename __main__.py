420feeda4aeea01ae504f371aeb99004c1abdb5d
MercuryBarium <marcus.brulls@gmail.com>
2020-02-02 19:09:01 +0100

    update

--- __main__.py
+++ __main__.py
@@ -1,18 +1,6 @@
 from model.infogathering.webscraper import scrape
 from model.matlista import vecka, dbconnector
-import pymysql
-from base64 import b64encode, b64decode
+import smtplib
 
-'''week = vecka(str(scrape('http://www.gladakocken.net/veckans-lunchmeny/', 'tr')))
-
-
-for day in week:
-    print(day.getDag())
-    for ratt in day.getRatter():
-        print(ratt)
-
-#db = pymysql.connect('localhost', 'foodtruck', 'qwerty123', 'matlista')
-
-'''
 database = dbconnector('localhost', 'pythonhttp', 'qwerty123', 'matlista')
-print(database.CreateNewUser('something@email.com', 'joe', 'ABC'))
+database.CreateNewUser('peter.brulls@gmail.com', 'Peter Brülls', 'abcd')
--- model/matlista.py
+++ model/matlista.py
@@ -73,12 +73,17 @@ class dbconnector:
             user, 
             password, 
             db, 
-            charset='utf8mb4', 
-            cursorclass=pymysql.cursors.DictCursor, 
-            autocommit=True)
+            charset     ='utf8mb4', 
+            cursorclass =pymysql.cursors.DictCursor, 
+            autocommit  =True)
         
         self.cur = connection.cursor()
 
+    #=======================================================================================
+    # If function returns 1, an unverified user has been created with a verificationtoken added to the vertokens table.
+    # If function returns 2, then the email adress has already been used and no new user has been created.
+    # If function returns 3, then the name has already been used and no new user has been created.
+    # If function returns 4, then both email and name has already been used and no new user has been created.
     def CreateNewUser(self, email, name, password):
         #B64 encoding is used to prevent SQL-injections
         email   = b64encode(email.encode('utf-8')).decode('utf-8')
@@ -94,7 +99,7 @@ class dbconnector:
         n       = len(self.cur.fetchall())
         
         if m == 0 and n == 0:
-            self.cur.execute('SELECT COUNT(*) FROM users;')
+            self.cur.execute('SELECT COUNT(*) FROM users;') 
             newid   = self.cur.fetchone()['COUNT(*)'] + 1
             print(newid)
             newuser = user_class(newid, email, name, hashAndSalt(password), 0)
@@ -106,19 +111,45 @@ class dbconnector:
                 newuser.password,
                 newuser.verified
             ))
-            return 'New unverified user successfully created!'
+            token = b64encode(os.urandom(16)).decode('utf-8')
+            self.cur.execute("INSERT INTO vertokens VALUES ('{}', '{}');".format(email, token))
+            return 1, token
         
         elif m > 0 and n == 0:
-            return 'Hm this email address has already been used'
+            return 2, 'None'
         
         elif n > 0 and m == 0:
-            return 'Hm this name has already been used'
+            return 3, 'None'
         
         else:
-            return 'Both email and name has already been used'
+            return 4, 'None'
 
+    #=======================================================================================
+    # If function returns 1, then the login initials are correct and the account is verified.
+    # If function returns 2, then the password is incorrect.
+    # If function returns 3, then the account is not verified
+    # If function returns 4, then the account does not exist.
+    def loginInitialsCompare(self, email, pwd):
+        email = b64encode(email.encode('utf-8')).decode('utf-8')
+        self.cur.execute("SELECT email, password, verified FROM users WHERE email = '{}';".format(email))
+        data = self.cur.fetchone()
+
+        if data['verified'] == 1:
+            if checkPW(pwd, data['password']):
+                return 1
+            
+            else:
+                return 2
+
+        elif data['verified'] == 0:
+            return 3
+
+        else:
+            return 4 
+    
+    
+        
         
 
-            
-
+        
 
