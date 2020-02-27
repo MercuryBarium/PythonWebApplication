056b43adc9f5881e68e0214c5cd295905a159dde
MercuryBarium <marcus.brulls@gmail.com>
2020-02-27 17:02:28 +0100

    yeee

--- test.py
+++ test.py
@@ -1,7 +1,40 @@
-#pythontestemail@protonmail.com
-#AdextraPL0CK#&556548
 import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
+from datetime import datetime
+from time import time
 
-msg = MIMEMultipart()
+
+config = open('./email.txt', 'r').readlines()
+
+email       = config[0]
+password    = config[1]
+
+def gettime():
+    timestamp   = time()
+    t           = datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+    return t
+
+class emailerSSL:
+    def __init__(self):
+        self.errorlog   = open('errorlog.txt', 'a')
+        self.server = smtplib.SMTP_SSL('protonmail.com', 465)
+        self.server.login(email, password)
+
+    def sendMail(self, recipient, subject, content):
+        body = '\r\n'.join([
+            'To: {}'.format(recipient),
+            'From: {}'.format(email),
+            'MIME-Version: 1.0',
+            'Content-type: text/html',
+            'Subject: {}'.format(subject),
+            content
+        ])
+
+        try:
+            self.server.sendmail(email, recipient, body)
+            print('successfully sent mail')
+        except Exception as e:
+            print(e)
+            self.errorlog.write('\n\n{}: {}'.format(gettime(), e))
+
+server = emailerSSL()
+server.sendMail('vebbe90@gmail.com', 'Hello World', '<h1>Yeet</h1>')
