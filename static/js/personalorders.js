1360501e83e79f06a52e22831c27a4377ad7982e
MercuryBarium <marcus.brulls@gmail.com>
2020-06-10 00:16:57 +0200

    You can now view your orders. Whoopie!

--- static/js/personalorders.js
+++ static/js/personalorders.js
@@ -26,6 +26,69 @@ later.setDate(now.getDate() + 7)
 startDate.value = formatDate(before)
 endDate.value = formatDate(later)
 
+function getOrCreateWeekDiv(id) {
+    if (document.getElementById(id)) {
+        return document.getElementById(id)
+    } else {
+        var ret = document.createElement('div')
+        ret.id = id
+        ret.className = 'container border border-primary m-2'
+
+        var weekAndYear = document.createElement('div')
+        weekAndYear.className = 'border-bottom'
+        var name = document.createElement('h2')
+        name.innerText = id
+        weekAndYear.append(name)
+        ret.append(weekAndYear)
+
+        var theOrders = document.createElement('div')
+        theOrders.className = 'row row-cols-1'
+        ret.append(theOrders)
+
+        $('#pageCONTENT').append(ret)
+        return ret
+    }
+}
+
+function genOrderCol(order) {
+    foodorders = order['foodorder']
+    menu = order['menu']
+    
+    var thisCol = document.createElement('div')
+    thisCol.className = 'col row-cols-1 m-1 border-bottom'
+
+    var row_1 = document.createElement('div')
+    row_1.className = 'col'
+    var name = document.createElement('h4')
+    name.innerText = order['day']
+    row_1.append(name)
+    thisCol.append(row_1)
+
+    var row_2 = document.createElement('div')
+    row_2.className = 'col'
+
+    for (let i = 0; i < foodorders.length; i++) {
+        var item = foodorders[i]['item']
+        var amount = foodorders[i]['amount']
+
+        var itemRow = document.createElement('div')
+        itemRow.className = 'col border-bottom m-1'
+
+        var menuText = document.createElement('h5')
+        menuText.innerText = menu[item] + ':  '
+
+        var span = document.createElement('span')
+        span.className = 'badge badge-secondary'
+        span.innerText = amount
+
+        menuText.append(span)
+        itemRow.append(menuText)
+        row_2.append(itemRow)
+    }
+    thisCol.append(row_2)
+    return thisCol
+}
+
 async function renderOrders() {
 
     const rawResponse = await fetch('/fetchorder', {
@@ -38,10 +101,14 @@ async function renderOrders() {
     })
 
     const cleanResponse = await rawResponse.json()
+    const fetchedOrders = cleanResponse['orders']
     console.log(cleanResponse)
 
-    if (cleanResponse['orders'].length > 0) {
-        console.log('Yes')
+    for (let i = fetchedOrders.length-1; i >= 0; i--) {
+        var year = fetchedOrders[i]['year']
+        var week = fetchedOrders[i]['weeknumber']
+
+        getOrCreateWeekDiv(`${year} Week: ${week}`).lastChild.append(genOrderCol(fetchedOrders[i]))
     }
 }
 
